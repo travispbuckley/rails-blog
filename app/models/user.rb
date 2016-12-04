261ae535924419194dc75486bf2ce3821955880b
travispbuckley <travispbuckley@lewisu.edu>
2016-12-04 10:30:42 -0600

    working on users, checking out to a new feature branch a little late

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
 	validates :username, :email, :hashed_password, { presence: true }
 	validates :username, :email, { uniqueness: true }
 
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(password)
+  	self.password == password
+  end
+
 end
